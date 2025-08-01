a8519da61c80780111c97cc240177b9e19d1d954
jinseop <jjs0110zz@naver.com>
2025-08-01 12:42:53 +0900

    step3-1

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -6,11 +6,14 @@ from schedule import Customer, Schedule
 from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
 
+NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
+ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
+CUSTOMER = Customer("Fake name", "010-1234-5678")
+
+
 def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가():
     #arrange
-    not_on_the_hour = datetime.strptime("2021/03/26 09:05","%Y/%m/%d %H:%M")
-    customer = Customer("Fake name","010-1234-5678")
-    schedule = Schedule(not_on_the_hour,1,customer)
+    schedule = Schedule(NOT_ON_THE_HOUR,1,CUSTOMER)
     booking_scheduler = BookingScheduler(3)
 
     #act and asser
@@ -20,9 +23,7 @@ def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가
 
 def test_예약은_정시에만_가능하다_정시인_경우_예약가능():
     #arrange
-    on_the_hour = datetime.strptime("2021/03/26 09:00","%Y/%m/%d %H:%M")
-    customer = Customer("Fake name","010-1234-5678")
-    schedule = Schedule(on_the_hour,1,customer)
+    schedule = Schedule(ON_THE_HOUR,1,CUSTOMER)
     booking_scheduler = BookingScheduler(3)
 
     #act
